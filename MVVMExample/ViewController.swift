f082435948f0b82e75a4677fc400b6cd90b5feb0
Zafer Çalışkan <caliskan.zafer@outlook.com>
2023-01-21 17:49:35 +0300

    Initial Commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Zafer Çalışkan on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
